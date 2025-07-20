b5e150506d69e48ed332f61b922320c24c621f27
Omarbas01 <omarbasab@gmail.com>
2025-07-20 10:34:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,7 +104,7 @@ if st.button("Search"):
             d365_col = "D365" if "D365" in df.columns else df.columns[10]
             markup_col = "MarkupCode" if "MarkupCode" in df.columns else df.columns[14]
             date_col = "Date" if "Scheduled" in df.columns else df.columns[15]
-            info_col = "التقييم" if "Info" in df.columns else None
+            info_col = "التقييم" if "Info" in df.columns else df. columns[2]
             part_img_col = "Picture of Part" if "Part Image" in df.columns else None
             problem_img_col = "Problem Image" if "Problem Image" in df.columns else None
             supervisor_col = "Supervisor" if "Supervisor" in df.columns else None
